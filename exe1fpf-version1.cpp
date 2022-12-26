1347ca91a52d44fcc589b83337ad464f975a6223
unknown <rogerio.melo.jr25@gmail.com>
2022-12-25 20:30:49 -0400

    Quarta commit

---
+++ exe1fpf-version1.cpp
@@ -0,0 +1,37 @@
+// Versão 1 da questão 1. Versão final.
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);
+
+using namespace std;
+
+int countSetBits(int X){
+    if(X == 0) // caso base
+       return 0;
+    else  
+      return (X & 1) + countSetBits(X >> 1); // soma o bit recursivamente e desloca o bit menos significativo 
+}
+int exercise1(int a, int b){ // parâmetros de entrada da função
+    int P;   //variáveis locais da função
+    P = a*b;
+    return countSetBits(P); // função que retorna a soma dos bits
+}
+
+int main(){_
+    int *A = new (nothrow) int; // Declaração dos ponteiros de inteiros e alocação de inteiros.
+    int *B = new (nothrow) int; 
+    
+    if(!A || !B) // verifica se há problema com a memória após a alocação.
+        return 0;
+    
+    cin >> *A >> *B; // Entrada dos números a serem multiplicados.
+    
+    if((*A)<0 || (*B)<0){
+        cout << "Apenas inteiros positivos!!" << endl;
+    }
+    else{
+        cout << exercise1(*A,*B) << endl; // Chamada da função e saída do resultado.
+    }
+    delete A; // libera a alocação;
+    delete B; // libera a alocação;
+    return 0;   
+}
